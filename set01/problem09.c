4141c7c91ef1e394c09c0ade856c3cbc662fc086
Musharrat Perveen <117891295+Musharrat-bsc@users.noreply.github.com>
2023-01-05 09:32:32 +0000

    solved

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,28 +1,34 @@
-#include <math.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
 float input();
 float square_root(float n);
 void output(float n, float sqrroot);
-int main()
-{
-  double n ,sqrroot;
-  n=input();
-  sqrroot=square_root(n);
-  output(n, sqrroot);
-}  
+
 float input()
 {
-  double n;
-  printf("Enter the number ");
-  scanf("%lf",&n);
+  float n;
+  printf("enter the number");
+  scanf("%f",&n);
   return n;
 }
 float square_root(float n)
 {
-  double sqrroot=sqrt(n);
-  return sqrroot;
+  float i=4;
+  while(fabs(i*i-n)/2 >0.0001)
+    {
+      i=(i+n/i)/2;
+    }
+  return i;
 }
-void output(float n ,float sqrroot)
+void output(float n, float sqrroot)
 {
-  printf("Square root of %.2lf =  %.2lf", n , sqrroot);
+  printf("square root of %.2f=%.2f",n,sqrroot);
+}
+int main()
+{
+  float n,sqrroot;
+  n=input();
+  sqrroot=square_root(n);
+  output(n,sqrroot);
+  return 0;
 }
