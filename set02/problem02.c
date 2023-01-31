8af69c9e7840592686cd84e0cef6611e812d7cbc
Musharrat Perveen <117891295+Musharrat-bsc@users.noreply.github.com>
2023-01-31 04:23:11 +0000

    solved

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,81 +1,40 @@
 #include<stdio.h>
-
 int input_side();
-int check_scalene(int a, int b, int c);
-void output(int a, int b, int c, int isscalene);
+int check_scalene(int a,int b,int c);
+void output(int a,int b,int c,int isscalene);
 
-int main()
+int input_side()
 {
-  int a,b,c,isscalene;
-  a=input();
-  b=input();
-  c=input();
-  check_scalene(a,b,c);
-  output(a,b,c,isscalene);
-}
-
-int input_side(){
   int x;
-  printf("Enter the sides :");
+  printf("enetr the side");
   scanf("%d",&x);
   return 0;
 }
-
-int check_scalene(int a, int b, int c){
-  if (a==b || b==c || c==a){
-    return 1;
-  }
-  else{
-    return 2;
-  }
-}
-
-void output(int a, int b, int c, int isscalene)
+int check_scalene(int a,int b,int c)
 {
-  if(isscalene == 1){
-    printf("The triangle with side %d %d and %d is not scalene",a,b,c);
+  int isscalene;
+  if(a!=b && b!=c)
+  {
+    printf("triangle is  scalene");
   }
-  else if(isscalene == 2){
-    printf("The triangle of side %d %d and %d is scalene",a,b,c);
-  }
-}#include<stdio.h>
-
-int input_side();
-int check_scalene(int a, int b, int c);
-void output(int a, int b, int c, int isscalene);
-
+  else
+  {
+    printf("traingle is not scalene");
+  }
+  return isscalene;
+}
+void output(int a,int b,int c,int isscalene)
+{
+  printf("the given of %d %d and %d is %d",a,b,c,isscalene);
+}
 int main()
 {
-  int a,b,c,isscalene;
-  a=input();
-  b=input();
-  c=input();
-  check_scalene(a,b,c);
+  int a,b,c;
+  a=input_side();
+  b=input_side();
+  c=input_side();
+  int isscalene;
+  isscalene=check_scalene(a,b,c);
   output(a,b,c,isscalene);
-}
-
-int input_side(){
-  int x;
-  printf("Enter the sides :");
-  scanf("%d",&x);
   return 0;
-}
-
-int check_scalene(int a, int b, int c){
-  if (a==b || b==c || c==a){
-    return 1;
-  }
-  else{
-    return 2;
-  }
-}
-
-void output(int a, int b, int c, int isscalene)
-{
-  if(isscalene == 1){
-    printf("The triangle with side %d %d and %d is not scalene",a,b,c);
-  }
-  else if(isscalene == 2){
-    printf("The triangle of side %d %d and %d is scalene",a,b,c);
-  }
 }
